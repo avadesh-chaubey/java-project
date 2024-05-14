05713e25b5db0025cc7075a9e6a8c5e157444a40
Avadesh Chaubey <avadesh.chaubey@genmills.com>
2024-05-14 14:42:31 +0530

    app deployed

--- hello.java
+++ hello.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.helloworld;
+
 
 /**
  *
  * @author Preetul
  */
-public class Helloworld {
+public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello World from My Java!");
