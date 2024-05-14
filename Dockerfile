9854476ec6b80b4d5218f99ae6a2acdac8342300
Avadesh Chaubey <avadesh.chaubey@genmills.com>
2024-05-14 14:54:11 +0530

    test1

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM openjdk:17-jdk-alpine
 COPY . /var/www/java  
 WORKDIR /var/www/java  
 RUN javac hello.java  
-CMD ["java", "Hello"]  
+CMD ["java", "hello"]  
--- hello.java
+++ hello.java
@@ -8,7 +8,7 @@
  *
  * @author Preetul
  */
-public class Hello {
+public class hello {
 
     public static void main(String[] args) {
         System.out.println("Hello World from My Java!");
